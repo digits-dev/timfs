aaa746b97158caf9ca694a145b175c6342e9268a
Mike Rodelas <mikerodelas@digits.ph>
2023-02-17 12:07:48 +0800

    adjust profile view

--- app/Http/Controllers/AdminCmsUsersController.php
+++ app/Http/Controllers/AdminCmsUsersController.php
@@ -74,7 +74,7 @@ class AdminCmsUsersController extends \crocodicstudio\crudbooster\controllers\CB
 
 		$data['page_title'] = trans("crudbooster.label_button_profile");
 		$data['row'] = CRUDBooster::first('cms_users',CRUDBooster::myId());		
-		$this->cbView('crudbooster::default.form',$data);				
+		$this->view('crudbooster::default.form',$data);				
 	}
 
 	public function hook_before_add(&$postdata) {        
