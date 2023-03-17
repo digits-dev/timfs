69795f51946706e0e5f883f4b3ac2c22f137ab80
patrickDigits <patrickpunzalan@digits.ph>
2023-03-17 15:33:47 +0800

    comment swal fire

--- resources/views/menu-items/add-menu-items.blade.php
+++ resources/views/menu-items/add-menu-items.blade.php
@@ -265,20 +265,20 @@
         allowClear: true
     });
 
-    function addBtn(event){
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {
-            if (result.isConfirmed) {
-                $('#form-add').submit()
-            }
-        })
-    }
+    // function addBtn(event){
+    //     Swal.fire({
+    //         title: 'Are you sure?',
+    //         text: "You won't be able to revert this!",
+    //         icon: 'warning',
+    //         showCancelButton: true,
+    //         confirmButtonColor: '#3085d6',
+    //         cancelButtonColor: '#d33',
+    //         confirmButtonText: 'Yes, delete it!'
+    //         }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             $('#form-add').submit()
+    //         }
+    //     })
+    // }
 </script>
 @endsection
