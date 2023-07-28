200a736c180cb1daa53a43ffa04f16ac2f5b8d8e
Fillinor <fillinorgunio@digits.ph>
2023-07-28 14:45:51 +0800

    adjust the menu ingredient export logic

--- app/Exports/MenuIngredientsExport.php
+++ app/Exports/MenuIngredientsExport.php
@@ -5,6 +5,7 @@ namespace App\Exports;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use App\Http\Controllers\AdminMenuItemsController;
 use CRUDBooster;
 use DB;
 
@@ -26,7 +27,8 @@ class MenuIngredientsExport  implements FromArray, WithHeadings
 
     public function array() : array
     {
-        return DB::table('menu_primary_ingredients')
+
+        $menu_query =  DB::table('menu_primary_ingredients')
             ->where('menu_items.status', 'ACTIVE')
             ->select(
                 'menu_primary_ingredients.tasteless_menu_code',
@@ -38,8 +40,20 @@ class MenuIngredientsExport  implements FromArray, WithHeadings
                 'menu_primary_ingredients.cost',
             )
             ->leftJoin('menu_items', 'menu_items.id', 'menu_primary_ingredients.menu_items_id')
-            ->orderBy('menu_items.menu_item_description')
-            ->get()
-            ->toArray();
+            ->orderBy('menu_items.menu_item_description');
+
+        if (in_array(CRUDBooster::myPrivilegeName(), ['Chef', 'Chef Assistant'])) {
+
+            $menu_ids = (new AdminMenuItemsController)->getMyMenuIds();
+
+            $menu_query->whereIn('menu_items.id', $menu_ids);
+
+            if (!$menu_ids) {
+                $menu_query->where('menu_items.id', null);
+            }
+        }
+
+        $menu_items = $menu_query->get()->toArray();
+        return $menu_items;
     }
 }
