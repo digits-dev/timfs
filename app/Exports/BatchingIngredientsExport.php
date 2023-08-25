ea1bd7a3395da26676962ad570c4fd99a96ebe8c
Fillinor <fillinorgunio@digits.ph>
2023-08-25 09:58:22 +0800

    add prep-qty, ingredient-qty, bi-code, nwi-code on export ingredients

--- app/Exports/BatchingIngredientsExport.php
+++ app/Exports/BatchingIngredientsExport.php
@@ -16,9 +16,10 @@ class BatchingIngredientsExport implements FromArray, WithHeadings
         return [
             'BATCHING ITEM CODE',
             'BATCHING ITEM DESCRIPTION',
-            'TASTELESS CODE',
+            'ITEM CODE',
             'INGREDIENT',
-            'QTY',
+            'PREPARATION QTY',
+            'INGREDIENT QTY',
             'UOM',
             'INGREDIENT COST',
         ];
@@ -31,13 +32,17 @@ class BatchingIngredientsExport implements FromArray, WithHeadings
             ->select(
                 'batching_primary_ingredients.bi_code',
                 'batching_primary_ingredients.ingredient_description',
-                'batching_primary_ingredients.tasteless_code',
+                DB::raw('COALESCE(batching_primary_ingredients.tasteless_code, batching_as_ingredient.bi_code, new_ingredients.nwi_code)'),
                 'batching_primary_ingredients.ingredient',
-                'batching_primary_ingredients.quantity',
+                'batching_ingredients_auto_compute.prep_qty',
+                'batching_ingredients_auto_compute.ingredient_qty',
                 'batching_primary_ingredients.uom',
                 'batching_primary_ingredients.cost',
             )
             ->leftJoin('batching_ingredients', 'batching_ingredients.id', 'batching_primary_ingredients.batching_ingredients_id')
+            ->leftJoin('batching_ingredients_auto_compute', 'batching_ingredients_auto_compute.id', 'batching_primary_ingredients.batching_ingredients_details_id')
+            ->leftJoin('new_ingredients', 'new_ingredients.id', 'batching_ingredients_auto_compute.new_ingredients_id')
+            ->leftJoin('batching_ingredients as batching_as_ingredient', 'batching_as_ingredient.id', 'batching_ingredients_auto_compute.batching_as_ingredient_id')
             ->orderBy('batching_ingredients.ingredient_description')
             ->get()
             ->toArray();
